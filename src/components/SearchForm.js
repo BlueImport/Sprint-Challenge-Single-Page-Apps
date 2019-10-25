07a39dab3f0ae5b02ee90fd1b10995241b3c664f
Kris <kristian.claire.101@gmail.com>
2019-10-25 09:16:48 -0700

    adds api and search bar

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,21 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+
+const SearchStyles = styled.section`
+display: flex;
+justify-content: center;
+`
 
 export default function SearchForm() {
- 
+  const [search, setSearch] = useState("");
   return (
-    <section className="search-form">
-     // Add a search form here
-    </section>
+    <SearchStyles className="search-form">
+      <div>
+          <label htmlFor="search">
+              <input type="text" id="search" value={search} placeholder="Search" onchange={event => setSearch (event.target.value)} />
+          </label>
+          <button className="search-btn">Submit</button>
+      </div>
+    </SearchStyles>
   );
 }
